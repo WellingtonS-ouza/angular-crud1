bbb5a96c7d27ed05c9d0a253dce40ca798318bc3
Wellington <wel.souza88@gmail.com>
2023-11-17 18:49:54 -0300

    Angular Material progress-spinner

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
@@ -17,7 +18,8 @@ import { CoursesComponent } from './courses/courses.component';
     CoursesRoutingModule,
     MatTableModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatProgressSpinnerModule
   ]
 })
 export class CoursesModule { }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http'
-import { first, tap } from 'rxjs';
+import { delay, first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API = "/assets/coures.json"
+  private readonly API = "/assets/courses.json"
 
   constructor(private httpClient: HttpClient) { }
 
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      tap(courses => console.log)
+      delay(15000),
+      tap(courses => console.log(courses))
     )
   }
 
